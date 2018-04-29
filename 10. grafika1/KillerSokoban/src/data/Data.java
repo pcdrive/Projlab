eb3af6ec09261706ba083bcc8573c083c579a41c
mocsi <amocsari@gmail.com>
2018-04-29 22:41:11 +0200

    data update
    jatek bevezetese data-be

--- 10. grafika1/KillerSokoban/src/data/Data.java
+++ 10. grafika1/KillerSokoban/src/data/Data.java
@@ -1,7 +1,10 @@
 package data;
 
+import sokoban.Jatek;
+
 public class Data {
 //    public static final int JatekosEro = 1;
     public static int PalyaX = 5;
     public static int PalyaY = 5;
+    public static Jatek jatek;
 }
