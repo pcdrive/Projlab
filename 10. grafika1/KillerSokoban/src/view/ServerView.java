4836125f804dcb235c261a15f073cbcbfd6be52a
mocsi <amocsari@gmail.com>
2018-04-29 19:02:41 +0200

    layout-k és view-k

---
+++ 10. grafika1/KillerSokoban/src/view/ServerView.java
@@ -0,0 +1,16 @@
+package view;
+
+/**
+ * A KillerSokoban egy jatek, ahol a jatekos egy raktari munkast alakit.
+ * A cel, hogy minden mas jatekosnal tobb megjelolt ladat tolj a helyere.
+ *
+ * @author Horvath Gergo
+ * @author Mocsari Andras
+ * @author Zsiros Balint
+ * @author Bottlik Judit
+ * @author Holub Csongor
+ * @version 1.0
+ * @since 2018.04.29.
+ */
+public class ServerView {
+}
