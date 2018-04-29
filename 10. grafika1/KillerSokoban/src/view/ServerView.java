3b18e00232aa23634210fd3fb9d0af8b3f1ea862
mocsi <amocsari@gmail.com>
2018-04-29 21:41:13 +0200

    serverview
    serverview be

--- 10. grafika1/KillerSokoban/src/view/ServerView.java
+++ 10. grafika1/KillerSokoban/src/view/ServerView.java
@@ -1,5 +1,16 @@
 package view;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import sokoban.Navigator;
+
+import java.io.IOException;
+
 /**
  * A KillerSokoban egy jatek, ahol a jatekos egy raktari munkast alakit.
  * A cel, hogy minden mas jatekosnal tobb megjelolt ladat tolj a helyere.
@@ -13,4 +24,34 @@ package view;
  * @since 2018.04.29.
  */
 public class ServerView {
+    @FXML
+    public TextField textfield_jatekos1nev;
+    @FXML
+    public TextField textfield_jatekos2nev;
+    @FXML
+    public TextField textfield_port;
+    @FXML
+    public Button button_megse;
+    @FXML
+    public Button button_start;
+
+    public void onButtonDownAction(ActionEvent actionEvent) {
+        if (actionEvent.getSource() == button_start) {
+            if (textfield_jatekos1nev.getText().isEmpty() || textfield_jatekos2nev.getText().isEmpty() || textfield_port.getText().isEmpty()) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Hiányos adatok!");
+                alert.setHeaderText(null);
+                alert.setContentText("Nem adott meg minden adatot!");
+                alert.show();
+                return;
+            }
+        } else if (actionEvent.getSource() == button_megse) {
+            try {
+                Parent root = FXMLLoader.load(getClass().getResource("/data/resources/layout/MenuView.fxml"));
+                Navigator.navigate(root);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
